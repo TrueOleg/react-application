3a01df3004072e5b4c6ad0eec536cd58e2e8c800
Oleg Zaichkin <trueolegzaichkin@gmail.com>
2018-06-11 08:38:21 +0300

    res

--- app/components/ListHeroes/index.js
+++ app/components/ListHeroes/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import axios from 'axios';
 
 class ListHeroes extends React.Component {
-  componentDidMount() {
-    axios({
-      method: 'get',
-      url: 'https://swapi.co/api/people/',
-    })
-      .then((response) => { console.log(response) });
+  constructor(props) {
+    super(props);
+    this.state = { results: [] };
   }
+  componentDidMount() {
+    axios.get('https://swapi.co/api/people/')
+      .then((res) => {
+        const { data } = res;
+        console.log('data', data);
+        return data;
+      })
+      .then((data) => {
+        this.setState({ results: data });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   render() {
-    return (<div />);
+    const { results } = this.state.results;
+    console.log('results', results);
+    return (
+      <ul>
+        {results.map(item => <li>{item.name}</li>)}
+      </ul>
+    )
   }
 }
 
