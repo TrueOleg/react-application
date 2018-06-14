ab1465d20c1db46090f30e40302d728e2f9f9c49
Oleg Zaichkin <trueolegzaichkin@gmail.com>
2018-06-14 07:28:23 +0300

    react

--- app/components/ListHeroes/index.js
+++ app/components/ListHeroes/index.js
@@ -4,18 +4,13 @@ import axios from 'axios';
 class ListHeroes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { results: [] };
+    this.state = { heroes: [] };
   }
   componentDidMount() {
     axios.get('https://swapi.co/api/people/')
       .then((res) => {
-        const { data } = res;
-        console.log('data', data);
-        return data;
-      })
-      .then((data) => {
-        this.setState({ results: data.results });
-        console.log('results', this.state.results);
+        this.setState({ heroes: res.data.results });
+        console.log('heroes', this.state.heroes);
       })
       .catch((err) => {
         console.error(err);
@@ -25,7 +20,7 @@ class ListHeroes extends React.Component {
   render() {
     return (
       <ul>
-        {this.state.results.map(item => <li key={item.name}>{item.name}</li>)}
+        {this.state.heroes.map(item => <li key={item.name}>{item.name}</li>)}
       </ul>
     )
   }
